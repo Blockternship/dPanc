bb8891cbcf442a5d5972e188a9e201cba61044a4
aasimniz95@gmail.com <aasimniz95@gmail.com>
2018-09-04 22:30:34 -0700

    Form and Web3 integration complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FormsPage from './Form';
+import Web3 from 'web3';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 class App extends Component {
   state = {
@@ -8,10 +13,24 @@ class App extends Component {
   };
 
   componentDidMount() {
+
+    const web3 = window.web3
+    if (typeof web3 !== 'undefined') {
+      // Use Mist/MetaMask's provider
+      const web3js = new Web3(web3.currentProvider);
+      console.log(web3js)
+    } else {
+      console.log('No web3? You should consider trying MetaMask!')
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      const web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+    // Now you can start your app & access web3 freely:
+    console.log('Cheers!')
+
     this.callApi()
       .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
-  }
+    }
 
   callApi = async () => {
     const response = await fetch('/api/helloworld');
@@ -28,8 +47,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to dPanc</h1>
+          <p className="App-desc">
+          dPanc is a tool that allows individuals with Type 1 Diabetes
+          to be able to gain more insight around their own data.
+          </p>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+          <p className="App-intro">{this.state.response}</p>
+
+          <FormsPage />
+
       </div>
     );
   }
