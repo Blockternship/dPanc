f4acf9e491ce5abfe935d674f7c1323c73d40acd
lu.kevin@berkeley.edu <kelu@paypal.com>
2018-09-17 23:39:48 -0700

    Post parsed data to IPFS via infura

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
+const bodyParser = require('body-parser')
 const upload = multer();
 
 const app = express();
@@ -12,9 +13,11 @@ app.get('/api/helloworld', (req, res) => {
   res.send({ express: 'Hello dPanc!' });
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
 // Enable Cross-Origin Resource Sharing
 app.use(cors());
+// Enable body in post requests
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.post('/upload', upload.any(), data_controller.parseCSV);
