930274f9160a1078bb4a7986d7c23e82c313cdde
Jakob Sandberg <jakobs@jakobs01.attlocal.net>
2018-09-23 20:31:45 -0700

    Implement long term average glucose guage, refactor with react highcharts pattern

---
+++ src/examples/solid-guage-config.js
@@ -0,0 +1,63 @@
+export const solidGuageConfig = {
+    chart: {
+        type: 'solidgauge'
+    },
+
+    title: null,
+
+    pane: {
+        center: ['50%', '85%'],
+        size: '140%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor: '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -150,
+            text: 'Hemoglobin A1C Estimate'
+        },
+        labels: {
+            y: 16
+        },
+        min: 5,
+        max: 12,
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    },
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [7.5],
+    }]
+};
